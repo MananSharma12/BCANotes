a04c966047187e44a7f18add7911755509b4919c
Manan Sharma <60072470+MananSharma12@users.noreply.github.com>
2020-07-30 12:22:00 +0530

    Commented script.js for now

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-mybutton = document.getElementById("myBtn");
+// mybutton = document.getElementById("myBtn");
 
-window.onscroll = function() { scrollFunction() };
+// window.onscroll = function() { scrollFunction() };
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     mybutton.style.display = "block";
+//   } else {
+//     mybutton.style.display = "none";
+//   }
+// }
 
-function topFunction() {
-    document.documentElement.scrollTop = 0; 
-}
+// function topFunction() {
+//     document.documentElement.scrollTop = 0; 
+// }
